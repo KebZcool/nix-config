42fb872004f580bfe4d5e2e0ba67dbee3e2d43e5
kebZcool <157246690+KebZcool@users.noreply.github.com>
2025-02-01 18:34:44 +0100

    Add pkgs and mangohud

--- gaming.nix
+++ gaming.nix
@@ -10,15 +10,11 @@
 
 environment.sessionVariables = {
       MANGOHUD_CONFIG = "control=mangohud,legacy_layout=0,horizontal,battery,time,time_format=%H\\:%M,gpu_stats,gpu_power,cpu_stats,ram,vram,fps,frametime=1,frame_timing=1,hud_no_margin,table_columns=14";
-   };
-
+      MANGOHUD = "1";
+ };
  # Steam
   programs.steam = {
    enable = true;
-   extraEnv = {
-       MANGOHUD = true;
-    };
-  };
    remotePlay.openFirewall = true; 
    dedicatedServer.openFirewall = true; 
    localNetworkGameTransfers.openFirewall = true;
