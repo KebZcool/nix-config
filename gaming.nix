27d2ccfbf189711f9a7784e494688c73fe6eb4da
kebZcool <157246690+KebZcool@users.noreply.github.com>
2025-02-02 10:20:21 +0100

    Update gaming.nix

--- gaming.nix
+++ gaming.nix
@@ -10,19 +10,20 @@
 
 environment.sessionVariables = {
       MANGOHUD_CONFIG = "control=mangohud,legacy_layout=0,horizontal,battery,time,time_format=%H\\:%M,gpu_stats,gpu_power,cpu_stats,ram,vram,fps,frametime=1,frame_timing=1,hud_no_margin,table_columns=14";
-      MANGOHUD = "1";
- };
+    }; 
+
  # Steam
   programs.steam = {
-   enable = true;
-   remotePlay.openFirewall = true; 
-   dedicatedServer.openFirewall = true; 
-   localNetworkGameTransfers.openFirewall = true;
- };
-
-  # Configuration de la journalisation pour Steam
-  services.journald.extraConfig = ''
-    DefaultLimitNOFILE=1048576
-  '';
+    enable = true;
+    package = pkgs.steam.override {
+       extraEnv = {
+         MANGOHUD = true;
+       };
+     };
+      remotePlay.openFirewall = true;
+      localNetworkGameTransfers.openFirewall = true;
+      dedicatedServer.openFirewall = true; 
+      extraCompatPackages = with pkgs; [ proton-ge-bin ];
+    };
 
 }
