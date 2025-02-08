7346e26953a7ce0539d508193f1cdbba427c9ae9
kebZcool <157246690+KebZcool@users.noreply.github.com>
2025-02-08 21:58:39 +0100

    Update boot.nix

--- boot.nix
+++ boot.nix
@@ -7,6 +7,16 @@
     loader.systemd-boot.enable = true;
     loader.efi.canTouchEfiVariables = true;
     kernelPackages = lib.mkDefault pkgs.linuxPackages_zen;
+    kernelParams = [
+      "quiet"
+      "splash"
+      "boot.shell_on_fail"
+      "loglevel=3"
+      "rd.systemd.show_status=false"
+      "rd.udev.log_level=3"
+      "udev.log_priority=3"
+    ];
+
     # Configuration de Plymouth
     plymouth = {
       enable = true;
@@ -18,19 +28,11 @@
         })
       ];
     };
+  };
 
     # Activer le "Silent Boot"
     consoleLogLevel = 0;
     initrd.verbose = false;
-    kernelParams = [
-      "quiet"
-      "splash"
-      "boot.shell_on_fail"
-      "loglevel=3"
-      "rd.systemd.show_status=false"
-      "rd.udev.log_level=3"
-      "udev.log_priority=3"
-    ];
-
+   
   };
 }
