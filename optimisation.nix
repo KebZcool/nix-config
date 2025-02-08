1aefd8fcd687685e393e2eb3a49fe85b361451d6
kebZcool <157246690+KebZcool@users.noreply.github.com>
2025-02-08 18:04:02 +0100

    Update optimisation.nix

--- optimisation.nix
+++ optimisation.nix
@@ -1,8 +1,12 @@
 { config,... }:
 
 {
+
+ # Fonctionnement de Davinci Resolve avec les GPU AMD 
+   hardware.amdgpu.opencl.enable = true; 
+
   # zRAM Configuration
-  zramSwap.enable = true;
+   zramSwap.enable = true;
 
   # Automatic Garbage collection
    nix.gc = {
