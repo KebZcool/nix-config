4816f80def0b1350b2b3867714bd7d79fecc2c96
kebZcool <157246690+KebZcool@users.noreply.github.com>
2025-02-09 11:51:34 +0100

    Fix wayland

--- kde.nix
+++ kde.nix
@@ -1,11 +1,19 @@
-
 { lib, config, pkgs, ... }:
 
 {
   # Activation de KDE
   services = {
     desktopManager.plasma6.enable = lib.mkDefault true;
-  };
+
+    # SDDM
+    displayManager = {
+      sddm = {
+        enable = true; # This line is necessary to enable SDDM
+        wayland.enable = true;
+        theme = "nord";
+      };
+    };  
+  };  
 
   # Required for SDDM
   qt.style = "breeze";
@@ -14,16 +22,8 @@
     polkit = {
       enable = true;
     };
+  };  
 
-  # SDDM
-    displayManager = {
-      sddm = {
-        enable = true;
-        theme = "nord";
-      };
-    };
-  };
- 
   documentation.nixos.enable = false;
 
   # Packages système
